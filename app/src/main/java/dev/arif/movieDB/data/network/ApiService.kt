6bb0cfc3b3d440e0e0d208077c8272d83d8c73f8
Md. Ariful ISlam <arifewucse2016@gmail.com>
2023-03-01 10:57:38 +0600

    initial

--- app/src/main/java/dev/arif/movieDB/data/network/ApiService.kt
+++ app/src/main/java/dev/arif/movieDB/data/network/ApiService.kt
@@ -20,7 +20,9 @@ interface ApiService {
     //Movies
 
     @GET("3/movie/upcoming?api_key=$API_KEY")
-    suspend fun getUpcomingMovies(@Query("language") language: String,@Query("page") page : Int): Response<Movie>
+    suspend fun getUpcomingMovies(
+        @Query("language") language: String,
+        @Query("page") page : Int): Response<Movie>
 
     @GET("3/movie/popular?api_key=$API_KEY")
     suspend fun getPopularMovies(@Query("language") language: String,@Query("page") page : Int): Response<Movie>
